e85ef76326947fdaf68ec7007d26c3a4e98e6b92
Mr. Musk Man <redactedontop>
2024-07-19 17:31:49 +0100

    Made code blocks use light mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
 	site: 'https://redactedontop.github.io',
 	integrations: [sitemap(), expressiveCode({
 		tabWidth: 4,
+		themes: ['github-light'],
 		styleOverrides: {
 			codeFontSize: '1rem'
 		}
