5bc467a292d03e78a723d3f87a67dc347c1a2819
Mr. Musk Man <redactedontop>
2024-07-19 10:05:38 +0100

    Changed shitty to silly

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Alexander's little corner";
-export const SITE_DESCRIPTION = 'Welcome to my shitty little blog...';
+export const SITE_DESCRIPTION = 'Welcome to my silly little blog...';
